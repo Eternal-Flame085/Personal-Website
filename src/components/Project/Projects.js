842501724376ca18e49eda8621a2ff89fa05e9f8
Roberto Basulto <roberto.basulto085@gmail.com>
2021-03-14 12:56:02 -0400

    In the process of migrating from bootstrap to semantic ui

--- src/components/Project/Projects.js
+++ src/components/Project/Projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Col, Row } from 'react-bootstrap';
+import { Container, Divider, Grid, Image, Segment } from 'semantic-ui-react'
 import './Projects.css'
 
 import pack_smart from '../../images/pack_smart.png'
@@ -11,8 +11,14 @@ class Projects extends Component {
     render() {
         return (
             <fragment>
-                <PackSmart />
-                <CarbonKnight />
+                <Container>
+                    <Segment>
+                        <PackSmart />
+                    </Segment>
+                    <Segment>
+                        <CarbonKnight />
+                    </Segment>
+                </Container>
             </fragment>
         )
     }
@@ -20,52 +26,51 @@ class Projects extends Component {
   
 function PackSmart() {
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <img src={pack_smart} alt="Pack Smart homepage" class="project_images" />
-                </Col>
-                <Col>
-                    <h1>Pack Smart</h1>
-                    <a href="https://github.com/Pack-Smart/pack-smart-be">
-                        <img src={github} length="30" height="30"></img>
-                    </a>
-                    <a href="https://packsmart.herokuapp.com/">
-                        <img src={heroku} length="30" height="30"></img>
-                    </a>
-                    <p>
-                    Pack Smart is a site that helps users pack for trips by giving them suggestions on items they might need for that trip based on a small survey.
+        <Grid columns={2} relaxed='very'>
+            <Grid.Column>
+                <Image src={pack_smart} />
+            </Grid.Column>
+            <Grid.Column>
+                <h1>
+                    Pack Smart
+                    <Image floated="right" src={github} size='mini' href="https://github.com/Pack-Smart/pack-smart-be" target='_blank' />
+                    <Image floated="right"src={heroku} size='mini' href="https://packsmart.herokuapp.com/" target='_blank' />
+                </h1>
 
-                    <br></br><br></br><b>Technologies used:</b> Python, Flask, SQLAlchemy, pytest, PostgreSQL, Heroku, Travis
-                    </p>
-                </Col>
-            </Row>
-        </Container>
+                <Divider clearing/>
+
+                <p>
+                Pack Smart is a site that helps users pack for trips by giving them suggestions on items they might need for that trip based on a small survey.
+                <Divider hidden />
+                <b>Technologies used:</b> Python, Flask, SQLAlchemy, pytest, PostgreSQL, Heroku, Travis
+                </p>
+            </Grid.Column>
+        </Grid>
     )
 }
 
 function CarbonKnight() {
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <h1>CarbonKnight</h1>
-                    <a href="https://github.com/Carbon-Knight">
-                        <img src={github} length="30" height="30"></img>
-                    </a>
-                    <a href="https://carbon-knight.herokuapp.com/">
-                        <img src={heroku} length="30" height="30"></img>
-                    </a>
-                    <p>
-                    CarbonKnight is an application where a user is able to input their travel data and see what their unique carbon footprint is over time.
-                    <br></br><br></br><b>Technologies used:</b> Ruby, Rails, GraphQL, Active Record, PostgreSQL, Heroku, Travis
-                    </p>
-                </Col>
-                <Col>
-                    <img src={carbon_knight} alt="Carbon Knight homepage" class="project_images" />
-                </Col>
-            </Row>
-        </Container>
+        <Grid columns={2} relaxed='very'>
+            <Grid.Column>
+                <h1>
+                    CarbonKnight
+                    <Image floated="right" src={github} size='mini' href="https://github.com/Carbon-Knight" target='_blank' />
+                    <Image floated="right"src={heroku} size='mini' href="https://carbon-knight.herokuapp.com/" target='_blank' />
+                </h1>
+
+                <Divider clearing/>
+
+                <p>
+                CarbonKnight is an application where a user is able to input their travel data and see what their unique carbon footprint is over time.
+                <Divider hidden />
+                <b>Technologies used:</b> Ruby, Rails, GraphQL, Active Record, PostgreSQL, Heroku, Travis
+                </p>
+            </Grid.Column>
+            <Grid.Column>
+                <Image src={carbon_knight} />
+            </Grid.Column>
+        </Grid>
     )
 }
 
