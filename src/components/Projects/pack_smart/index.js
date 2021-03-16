d5f40cb2f58efc46a6f2d913cf10d782e6620d80
Roberto Basulto <roberto.basulto085@gmail.com>
2021-03-16 17:10:19 -0400

    Updated project technologies to use lists

--- src/components/Projects/pack_smart/index.js
+++ src/components/Projects/pack_smart/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Divider, Grid, Image, Segment, Button, Icon } from 'semantic-ui-react'
+import { Divider, Grid, Image, Button, Icon, List, Header } from 'semantic-ui-react'
 
 import landing_page from './images/landing_page.png'
 import how_it_works from './images/how_it_works.png'
@@ -82,10 +82,59 @@ class PackSmart extends Component {
                     <Divider clearing/>
     
                     <p>
-                    Pack Smart is a site that helps users pack for trips by giving them suggestions on items they might need for that trip based on a small survey.
-                    <Divider hidden />
-                    <b>Technologies used:</b> Python, Flask, SQLAlchemy, pytest, PostgreSQL, Heroku, Travis
+                        Pack Smart is a site that helps users pack for trips by giving them suggestions on items they might need for that trip based on a small survey.
                     </p>
+                    <Divider hidden />
+
+                    <Grid container>
+                        <Grid.Row>
+                            <Header as='h4'>Main technologies used:</Header>
+                        </Grid.Row>
+
+                        <Grid.Row columns={2}>
+                            <Grid.Column width='5'>
+                                <div>
+                                    <List relaxed>
+                                        <List.Item>
+                                            <Icon name='right triangle' />
+                                            Python
+                                        </List.Item>
+
+                                        <List.Item>
+                                            <Icon name='right triangle' />
+                                            Flask
+                                        </List.Item>
+
+                                        <List.Item>
+                                            <Icon name='right triangle' />
+                                            SQLAlchemy
+                                        </List.Item>                                   
+                                    </List>
+                                </div>
+                            </Grid.Column>
+
+                            <Grid.Column>
+                                <div>
+                                    <List relaxed>
+                                        <List.Item>
+                                            <Icon name='right triangle' />
+                                            PostgreSQL
+                                        </List.Item>
+
+                                        <List.Item>
+                                            <Icon name='right triangle' />
+                                            Heroku
+                                        </List.Item> 
+
+                                        <List.Item>
+                                            <Icon name='right triangle' />
+                                            TravisCI
+                                        </List.Item>                                   
+                                    </List>
+                                </div>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
                 </Grid.Column>
             </Grid>
         )
