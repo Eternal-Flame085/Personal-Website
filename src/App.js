81d652c2dea3d073b48c9e91f4af106a737b31f4
Roberto Basulto <roberto.basulto085@gmail.com>
2021-03-16 13:02:50 -0400

    Refactored out the navbar and changed the Menu item as Link from react router

--- src/App.js
+++ src/App.js
@@ -4,41 +4,16 @@ import {
   Switch,
   Route
 } from "react-router-dom";
-import { Menu, Image, Container, Grid } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
-import logo from './images/wide_logo.png';
-
+import NavBar from './components/NavBar/NavBar'
 import LandingPage from './components/LandingPage/LandingPage'
 import Projects from "./components/Project/Projects"
 
 export default function App() {
   return (
     <Router>
-      <Container fluid>
-        <Grid columns={2}>
-
-          <Grid.Column>
-            <Image src={logo} fluid size="small" href="/" />
-          </Grid.Column>
-
-          <Grid.Column>
-            <Menu secondary floated='right'>
-              <Menu.Item href="/">
-                Home
-              </Menu.Item>
-              
-              <Menu.Item href="/projects">
-                Projects
-              </Menu.Item>
-              
-              <Menu.Item href="/users">
-                Some Link
-              </Menu.Item>
-            </Menu>
-          </Grid.Column>
-        </Grid>
-      </Container>
+      <NavBar />
 
       <Switch>
         <Route path="/projects">
