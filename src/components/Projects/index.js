f228e722992ed165cb5209cd59e483e47f236711
Roberto Basulto <roberto.basulto085@gmail.com>
2021-03-17 16:38:53 -0400

    Changed the class to a Hook

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,21 +4,19 @@ import { Container, Segment} from 'semantic-ui-react'
 import PackSmart from './pack_smart'
 import CarbonKnight from './carbon_knight'
 
-class Projects extends Component {
-    render() {
-        return (
-            <fragment>
-                <Container style={{ paddingTop:20 }}>
-                    <Segment>
-                        <PackSmart />
-                    </Segment>
-                    <Segment>
-                        <CarbonKnight />
-                    </Segment>
-                </Container>
-            </fragment>
-        )
-    }
+const Projects = () => {
+    return (
+        <fragment>
+            <Container style={{ paddingTop:20 }}>
+                <Segment>
+                    <PackSmart />
+                </Segment>
+                <Segment>
+                    <CarbonKnight />
+                </Segment>
+            </Container>
+        </fragment>
+    )
 }
 
 export default Projects
