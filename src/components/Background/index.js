b0759a473f56680f69ef884007946aa97b107523
Roberto Basulto <roberto.basulto085@gmail.com>
2021-03-18 01:27:55 -0400

    Created Background hook

---
+++ src/components/Background/index.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+
+const Background = () => {
+    return (
+        <Particles style={{ minHeight: '100vh', minWidth: '100vw'}}
+            params={{
+                particles: {
+                    number: {
+                        value: 80,
+                        density: {
+                            enable: true,
+                            value_area: 800
+                        }
+                    },
+                    color: {
+                        value: "#ffffff"
+                    },
+                    shape: {
+                        type: "circle",
+                        stroke: {
+                            width: 2,
+                            color: "#000000"
+                        }
+                    },
+                    size: {
+                        value: 3,
+                        random: true,
+                        anim: {
+                            enable: false,
+                            speed: 40,
+                            size_min: 0.1,
+                            sync: false
+                        }
+                    },
+                    line_linked: {
+                        enable: true,
+                        distance: 150,
+                        color: '#ffffff',
+                        opacity: 0.6,
+                        width: 1
+                    },
+                    move: {
+                        enable: true,
+                        speed: 5,
+                        direction: 'none',
+                        random: false,
+                        straight: false,
+                        out_mode: 'out',
+                        bounce: false,
+                        attract: {
+                            enable: false,
+                            rotateX: 600,
+                            rotateY: 1200
+                        }
+                    }
+                },
+                interactivity: {
+                    detect_on: "canvas",
+                    events: {
+                        onhover: {
+                            enable: true,
+                            mode: 'grab'
+                        },
+                        onclick: {
+                            enable: true,
+                            mode: 'push'
+                        },
+                        resize: true
+                    },
+                    modes: {
+                        grab: {
+                            distance: 120,
+                            line_linked: {
+                            opacity: 0.5504691130161662
+                            }
+                        },
+                    }
+                }
+            }}
+        />
+    )
+};
+
+export default Background
