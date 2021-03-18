ec27b89192e17f5a2eba5fc0bd3ff69d5f217a14
Roberto Basulto <roberto.basulto085@gmail.com>
2021-03-18 01:34:28 -0400

    Used divs to divide and overlay components to implement the background

--- src/App.js
+++ src/App.js
@@ -4,29 +4,42 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import { Image } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
+import Background from './components/Background';
 import NavBar from './components/NavBar';
 import LandingPage from './components/LandingPage';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
+import BackgroundImage from './images/background.jpg'
 
 export default function App() {
   return (
-    <Router>
-      <NavBar />
+    <div style={{ position: 'relative', overflow: "hidden" }}>
+      <Router>
+        <div style={{ position: 'absolute'}}>
+          <Image fluid src={BackgroundImage} style={{zIndex: -12}}/>
+        </div>
 
-      <Switch>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-        <Route path="/resume">
-          <Resume />
-        </Route>
-        <Route path="/">
-          <LandingPage />
-        </Route>
-      </Switch>
-    </Router>
+        <div style={{ position: 'absolute'}}>
+          <Background />
+        </div>
+
+
+        <NavBar />
+        <Switch>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/resume">
+            <Resume />
+          </Route>
+          <Route path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
