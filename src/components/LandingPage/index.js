79c72edf91cfab0ec665a6a1fc59060b7c556d62
Roberto Basulto <roberto.basulto085@gmail.com>
2021-03-18 22:08:27 -0400

    Rebuild the landing page now its a little cleaner and better to manage the styling

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,82 +1,76 @@
 import React from 'react';
-import { Header, Grid, Container, Image, List, Icon} from 'semantic-ui-react'
+import { Header, Grid, Container, Image, List, Icon, Segment } from 'semantic-ui-react'
+
 const LandingPage = () => {
     return (
-        <Container style={{paddingTop: 100, height: '100vh'}}>
-            <Grid centered stretched columns={2} divided verticalAlign='middle'>
-                <Grid.Row>
-                    <Grid.Column stretched width="8">
-                        <Grid container>
-                            <Grid.Row>
-                                <div>
-                                    <Header as='h2'>About Me</Header>
-                                    <p> Hello! I'm Roberto Basulto, a software developer currently based in Miami, FL.</p>
-                                    <p>
-                                        I have always been into video games, it was a passion that I wanted to follow.
-                                        My experience with programming was a dual enrollment course during high school. 
-                                        From there I signed up for a game developer course in college and was introduced to C++ and Java. 
-                                        I quickly found out I disliked the Art courses I needed to take alongside the programming languages.
-                                    </p>
-                                    <p>
-                                        Shortly after I was introduced to Turing, From the moment I started Turing, I have enjoyed learning the 
-                                        Ruby language and working with my peers to get through the rigorous program. 
-                                        I am very eager to continue my career as an important contributor to an organization. <br/><br/>
-                                    </p>
-                                    <Header as='h4'>Recent technologies I have worked with: </Header>
-                                </div>
-                            </Grid.Row>
+        <Container style={{width:'70%', paddingTop:'5%'}}>
+            <Segment compact style={{backgroundColor:'#ffffffB3'}}>
+                <Grid divided> 
+                    <Grid.Row>
+                        <Grid.Column width='11'>
+                        <Header as='h2'>About Me</Header>
+                            <p> Hello! I'm Roberto Basulto, a software developer currently based in Miami, FL.</p>
+                            <p>
+                                I have always been into video games, it was a passion that I wanted to follow.
+                                My experience with programming was a dual enrollment course during high school. 
+                                From there I signed up for a game developer course in college and was introduced to C++ and Java. 
+                                I quickly found out I disliked the Art courses I needed to take alongside the programming languages.
+                            </p>
+                            <p>
+                                Shortly after I was introduced to Turing, From the moment I started Turing, I have enjoyed learning the 
+                                Ruby language and working with my peers to get through the rigorous program. 
+                                I am very eager to continue my career as an important contributor to an organization.
+                            </p>
 
-                            <Grid.Row columns={2}>
+                            <Header as='h4'>Recent technologies I have worked with: </Header>
+
+                            <Grid relaxed>
                                 <Grid.Column width='5'>
-                                    <div>
-                                        <List relaxed>
-                                            <List.Item>
-                                                <Icon name='right triangle' />
-                                                Ruby On Rails
-                                            </List.Item>
+                                    <List relaxed>
+                                        <List.Item>
+                                            <Icon name='right triangle' />
+                                            Ruby On Rails
+                                        </List.Item>
 
-                                            <List.Item>
-                                                <Icon name='right triangle' />
-                                                GraphQL
-                                            </List.Item>
+                                        <List.Item>
+                                            <Icon name='right triangle' />
+                                            GraphQL
+                                        </List.Item>
 
-                                            <List.Item>
-                                                <Icon name='right triangle' />
-                                                PostgreSQL
-                                            </List.Item>                                   
-                                        </List>
-                                    </div>
+                                        <List.Item>
+                                            <Icon name='right triangle' />
+                                            PostgreSQL
+                                        </List.Item>                                   
+                                    </List>
                                 </Grid.Column>
 
-                                <Grid.Column>
-                                    <div>
-                                        <List relaxed>
-                                            <List.Item>
-                                                <Icon name='right triangle' />
-                                                Python
-                                            </List.Item>
+                                <Grid.Column width='7'>
+                                    <List relaxed>
+                                        <List.Item>
+                                            <Icon name='right triangle' />
+                                            Python
+                                        </List.Item>
 
-                                            <List.Item>
-                                                <Icon name='right triangle' />
-                                                Flask
-                                            </List.Item> 
+                                        <List.Item>
+                                            <Icon name='right triangle' />
+                                            Flask
+                                        </List.Item> 
 
-                                            <List.Item>
-                                                <Icon name='right triangle' />
-                                                SQL | Active Record | SQLAlchemy
-                                            </List.Item>                                   
-                                        </List>
-                                    </div>
+                                        <List.Item>
+                                            <Icon name='right triangle' />
+                                            SQL | Active Record | SQLAlchemy
+                                        </List.Item>                                   
+                                    </List>
                                 </Grid.Column>
-                            </Grid.Row>
-                        </Grid>
-                    </Grid.Column>
+                            </Grid>
+                        </Grid.Column>
 
-                    <Grid.Column width="5">
-                        <Image rounded centered size="medium" src="https://avatars.githubusercontent.com/u/63683549?s=400&u=2a570652ac4f72944a49c64842702154c7466b5c&v=4" />
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
+                        <Grid.Column width='5'floated='right' verticalAlign='middle'>
+                            <Image rounded centered fluid src="https://avatars.githubusercontent.com/u/63683549?s=400&u=2a570652ac4f72944a49c64842702154c7466b5c&v=4" />
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Segment>
         </Container>
     )
 }
