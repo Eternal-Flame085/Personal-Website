3b90daaf6fd5901226c995bfa6d7496210d77f2f
Roberto Basulto <roberto.basulto085@gmail.com>
2021-03-26 13:17:53 -0400

    Reverted changes to the centering of projects page because it broke the view in smaller screens

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -7,15 +7,7 @@ import CarbonKnight from './carbon_knight'
 const Projects = () => {
     return (
         <fragment>
-            <Container style={{ 
-                paddingBottom: '3%', 
-                margin: 0, 
-                position: 'absolute', 
-                top:'50%', 
-                left:'50%', 
-                msTransform: 'translate(-50%, -50%)', 
-                transform: 'translate(-50%, -50%)'
-            }}>
+            <Container>
                 <Segment raised style={{ backgroundColor:'#ffffffCC' }}>
                     <PackSmart />
                 </Segment>
