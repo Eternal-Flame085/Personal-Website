00e5c6c7488505677ba6407703f1878d7b3381be
Roberto Basulto <roberto.basulto085@gmail.com>
2021-03-25 22:36:55 -0400

    corrected route to reume pdf

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, Embed } from 'semantic-ui-react'
 
-import ResumePDF from './RobertoBasultoResume'
+import ResumePDF from './RobertoBasultoResume.pdf'
 
 const Resume = () => {
     return (
