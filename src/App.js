ef280c7f5de9e6bc54627efb8941d2d8f7dee404
Roberto Basulto <roberto.basulto085@gmail.com>
2021-06-04 17:33:48 -0400

    Renamed navbar to appbar and converted it from semantic ui to material ui

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import {
   Switch,
   Route
 } from "react-router-dom";
-import { Image, Container } from 'semantic-ui-react';
+import { Image } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
 import Background from './components/Background';
-import NavBar from './components/NavBar';
+import NavBar from './components/AppBar/AppBar';
 import LandingPage from './components/LandingPage';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
@@ -17,16 +17,10 @@ import BackgroundImage from './images/background.jpg'
 
 const App = () => {
   return (
-    <Container fluid style={{ position: 'relative', overflow: "hidden", minHeight:'100vh' }}>
       <Router>
+        {/* <Image fluid src={BackgroundImage}/> */}
 
-        <Container fluid style={{ position: 'absolute'}}>
-          <Image fluid src={BackgroundImage}/>
-        </Container>
-
-        <Container style={{ position: 'absolute', inHeight:'100vh'}}>
-          <Background />
-        </Container>
+        {/* <Background /> */}
 
         <NavBar />
 
@@ -49,7 +43,6 @@ const App = () => {
         </Switch>
 
       </Router>
-    </Container>
   );
 }
 
---
+++ src/components/AppBar/AppBar.js
@@ -0,0 +1,108 @@
+import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
+
+import logo from '../../images/logo.png';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Link from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    boxShadow: 'none'
+  },
+  logo: {
+    maxWidth: '80px',
+    marginRight: '10px'
+  },
+  logoLink: {
+    flexGrow: 1,
+  }
+}))
+
+function DesktopMenu(props) {
+  return (
+    <>
+      {props.menuLinks.map((menuLink) => {
+        const { menuTitle, pageUrl } = menuLink;
+        return (
+          <Button variant='text' color="primary" component={RouterLink} to={pageUrl}>
+            {menuTitle}
+          </Button>
+        )
+      })}
+    </>
+  )
+}
+
+const NavBar = () => {
+  const classes = useStyles();
+  const menulinks = [
+    {menuTitle: "Home", pageUrl: "/"},
+    {menuTitle: "Projects", pageUrl: "/projects"},
+    {menuTitle: "Experience", pageUrl: "/experience"}
+  ]
+
+  return (
+    <AppBar position="fixed" color="transparent" className={classes.root}>
+      <Toolbar>
+        <Link className={classes.logoLink} component={RouterLink} to="/">
+          <img src={logo} alt="logo" className={classes.logo} />
+        </Link>
+
+        <DesktopMenu menuLinks={menulinks} />
+      </Toolbar>
+    </AppBar>
+
+
+
+
+
+    // <Menu style={{ marginTop:0 }} secondary inverted borderless>
+    //   <Menu.Item
+    //     name='logo'
+    //     content={
+    //       <Image
+    //         src={logo}
+    //         size='tiny'
+    //         as={Link}
+    //         to={'/'} 
+    //       />
+    //     }
+    //   />
+      
+    //   <Menu.Item
+    //     as={Link} to="/" 
+    //     name='About'
+    //     position='right'
+    //   />
+      
+    //   <Menu.Item  
+    //     as={Link} to="/projects"
+    //     name='Projects'
+    //   />
+
+    //   <Menu.Item 
+    //     as={Link} to="/experience"
+    //     name='Experience'
+    //   />
+      
+    //   <Menu.Item 
+    //     as={Link} to="/resume"
+    //     name='Resume'
+    //   />
+
+    //   <Dropdown item text="Contact" basic>
+    //     <Dropdown.Menu direction="left">
+    //       <Dropdown.Item icon='github' text='GitHub' target='_blank' href="https://github.com/Eternal-Flame085" />
+    //       <Dropdown.Item icon='linkedin' text='LinkedIn' target='_blank' href="https://www.linkedin.com/in/roberto-basulto/" />
+    //       <Dropdown.Item icon='google' text='Email' target='_blank' href="mailto:roberto.basulto085@gmail.com" />
+    //     </Dropdown.Menu>
+    //   </Dropdown>
+    // </Menu>
+  )
+}
+
+export default NavBar
