c8a0f172ca8f8d552a303689baf4b87c27120c0a
Roberto Basulto <roberto.basulto085@gmail.com>
2021-06-05 16:53:43 -0400

    Renamed index.js to Project.js

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/container";
+import Grid from "@material-ui/core/grid"
+import projects from "./ProjectData.json"
+import ProjectTemplate from "./ProjectTemplate";
+
+const useStyles = makeStyles((theme) => ({
+  cardImage: {
+    width: "40%",
+    height: 340,
+  },
+  projectHeader: {
+    flexGrow: 1,
+    marginLeft: "15px",
+  },
+  chips: {
+    display: "flex",
+    // justifyContent: 'center',
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(0.5),
+    },
+  },
+  paragraphStyle: {
+    marginLeft: "15px",
+  },
+}));
+
+const Projects = () => {
+  const classes = useStyles();
+  return (
+      <Container>
+        <Grid container direction="column" spacing="2">
+          {projects.map((project) => {
+            return (
+              <ProjectTemplate classes={classes} project={project}/>
+            );
+          })}
+        </Grid>
+      </Container>
+  );
+};
+
+export default Projects;
