c1e08db2e76285a05afb9bb849320468b8f8c98e
Roberto Basulto <roberto.basulto085@gmail.com>
2021-06-08 19:16:37 -0400

    Updated background import

--- src/App.js
+++ src/App.js
@@ -4,23 +4,19 @@ import {
   Switch,
   Route
 } from "react-router-dom";
-import { Image } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
-import Background from './components/Background';
+import Background from './components/Background/Background';
 import NavBar from './components/AppBar/AppBar';
 import LandingPage from './components/LandingPage/Home';
 import Projects from './components/Projects/Projects';
 import Experience from './components/Experience/Experience';
 import Resume from './components/Resume';
-import BackgroundImage from './images/background.jpg'
 
 const App = () => {
   return (
       <Router>
-        {/* <Image fluid src={BackgroundImage} /> */}
-        {/* <Background /> */}
-
+        <Background />
         <NavBar />
 
         <Switch>
