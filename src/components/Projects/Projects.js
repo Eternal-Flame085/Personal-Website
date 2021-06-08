47cda9a60344b2e6d32ab7c00a800d3d41f03981
Roberto Basulto <roberto.basulto085@gmail.com>
2021-06-07 20:22:31 -0400

    added ; to imports

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Container, Grid } from '@material-ui/core'
-import projects from "./ProjectData.json"
+import { Container, Grid } from '@material-ui/core';
+import projects from "./ProjectData.json";
 import ProjectTemplate from "./ProjectTemplate";
 
 const useStyles = makeStyles((theme) => ({
