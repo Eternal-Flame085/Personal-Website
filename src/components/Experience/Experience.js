2725936df6336d36ef9c385d21e0b6cbcd20b48c
Roberto Basulto <roberto.basulto085@gmail.com>
2021-06-07 20:24:26 -0400

    Redid the experiences page with material ui and created the experience templete for every experience i had

---
+++ src/components/Experience/Experience.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, Grid } from '@material-ui/core';
+
+import experienceData from "./Experience.json";
+import ExperienceTemplate from "./ExperienceTemplate";
+
+const useStyles = makeStyles(() => ({
+    image: {
+        objectFit: "contain",
+        width:"100%",
+        height:"100%",
+        paddingTop: "15px",
+        paddingBottom: "15px"
+    },
+    imageGridContainer: {
+        height:"350px"
+    },
+    experienceHeader: {
+      marginLeft: "15px",
+    },
+    experienceSubheader: {
+        flexGrow: 1,
+        marginLeft: "15px",
+    },
+    experienceYears: {
+        marginRight: "15px"
+    },
+    toolbar: {
+        marginTop: "-15px", 
+        marginBottom: "-10px"
+    },
+    paragraphStyle: {
+      marginLeft: "15px",
+      marginRight: "15px"
+    },
+  }));
+
+const Experience = () => {
+    const classes = useStyles();
+    return (
+        <Container>
+            <Grid container direction="column" spacing="2">
+                {
+                    experienceData.map((experience) => {
+                        return (
+                            <ExperienceTemplate classes={classes} experience={experience} />
+                        )
+                    })
+                }
+            </Grid>
+      </Container>
+    )
+}
+
+export default Experience
