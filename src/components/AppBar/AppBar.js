46426ec3d7ae9f3144fbb4eb3acd4753066c4961
Roberto Basulto <roberto.basulto085@gmail.com>
2021-06-18 17:56:30 -0400

    Cleaned up the navbar and added dropdown with links to diferent sites (Linkedin, github)

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import { Link as RouterLink } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
-
-import logo from '../../images/logo.png';
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button'
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Popper from '@material-ui/core/Popper';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import MenuList from '@material-ui/core/MenuList';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import Typography from '@material-ui/core/Typography';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import EmailIcon from '@material-ui/icons/Email';
 
-const useStyles = makeStyles((theme) => ({
+import logo from '../../images/logo.png';
+
+
+const useStyles = makeStyles(() => ({
   root: {
     flexGrow: 1,
     boxShadow: 'none'
@@ -19,20 +33,88 @@ const useStyles = makeStyles((theme) => ({
   },
   logoLink: {
     flexGrow: 1,
+  },
+  navColor: {
+    color: 'white',
+    textTransform: 'none'
   }
 }))
 
 function DesktopMenu(props) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+  const [placement, setPlacement] = React.useState();
+
+  const handleClick = (newPlacement) => (event) => {
+    setAnchorEl(event.currentTarget);
+    setOpen((prev) => placement !== newPlacement || !prev);
+    setPlacement(newPlacement);
+  };
+
+  const handleClickAway = () => {
+    setOpen(false);
+  };
   return (
     <>
+      <Popper open={open} anchorEl={anchorEl} placement={placement} transition>
+        {({ TransitionProps }) => (
+          <Grow {...TransitionProps} timeout={350}>
+            <ClickAwayListener onClickAway={handleClickAway}>
+            <Paper>
+              <MenuList id="menu-list-grow">
+                <a target='_blank' href="https://www.linkedin.com/in/roberto-basulto/">
+                  <MenuItem>
+                    <ListItemIcon style={{marginRight:-30}}>
+                      <LinkedInIcon />
+                    </ListItemIcon>
+
+                    <Typography>
+                      LinkedIn
+                    </Typography>
+                  </MenuItem>
+                </a>
+
+                <a target='_blank' href="https://github.com/Eternal-Flame085">
+                  <MenuItem>
+                    <ListItemIcon style={{marginRight:-30}}>
+                      <GitHubIcon />
+                    </ListItemIcon>
+
+                    <Typography>
+                      GitHub
+                    </Typography>
+                  </MenuItem>
+                </a>
+
+                <a target='_blank' href="mailto:roberto.basulto085@gmail.com">
+                  <MenuItem>
+                    <ListItemIcon style={{marginRight:-30}}>
+                      <EmailIcon />
+                    </ListItemIcon>
+
+                    <Typography>
+                      Email
+                    </Typography>
+                  </MenuItem>
+                </a>
+              </MenuList>
+            </Paper>
+            </ClickAwayListener>
+          </Grow>
+        )}
+      </Popper>
+
       {props.menuLinks.map((menuLink) => {
         const { menuTitle, pageUrl } = menuLink;
         return (
-          <Button variant='text' color="primary" component={RouterLink} to={pageUrl}>
+          <Button className={props.classes.navColor} variant='text' component={RouterLink} to={pageUrl}>
             {menuTitle}
           </Button>
         )
       })}
+      <IconButton onClick={handleClick('bottom')}>
+        <MenuIcon className={props.classes.navColor} />
+      </IconButton>
     </>
   )
 }
@@ -53,56 +135,9 @@ const NavBar = () => {
           <img src={logo} alt="logo" className={classes.logo} />
         </Link>
 
-        <DesktopMenu menuLinks={menulinks} />
+        <DesktopMenu classes={classes} menuLinks={menulinks}/>
       </Toolbar>
     </AppBar>
-
-
-
-
-
-    // <Menu style={{ marginTop:0 }} secondary inverted borderless>
-    //   <Menu.Item
-    //     name='logo'
-    //     content={
-    //       <Image
-    //         src={logo}
-    //         size='tiny'
-    //         as={Link}
-    //         to={'/'} 
-    //       />
-    //     }
-    //   />
-      
-    //   <Menu.Item
-    //     as={Link} to="/" 
-    //     name='About'
-    //     position='right'
-    //   />
-      
-    //   <Menu.Item  
-    //     as={Link} to="/projects"
-    //     name='Projects'
-    //   />
-
-    //   <Menu.Item 
-    //     as={Link} to="/experience"
-    //     name='Experience'
-    //   />
-      
-    //   <Menu.Item 
-    //     as={Link} to="/resume"
-    //     name='Resume'
-    //   />
-
-    //   <Dropdown item text="Contact" basic>
-    //     <Dropdown.Menu direction="left">
-    //       <Dropdown.Item icon='github' text='GitHub' target='_blank' href="https://github.com/Eternal-Flame085" />
-    //       <Dropdown.Item icon='linkedin' text='LinkedIn' target='_blank' href="https://www.linkedin.com/in/roberto-basulto/" />
-    //       <Dropdown.Item icon='google' text='Email' target='_blank' href="mailto:roberto.basulto085@gmail.com" />
-    //     </Dropdown.Menu>
-    //   </Dropdown>
-    // </Menu>
   )
 }
 
