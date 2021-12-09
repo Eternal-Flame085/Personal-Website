c899644dc7cde1ed3c1a731588635826b7061e78
Roberto Basulto <Roberto.Basulto085@gmail.com>
2021-12-09 15:38:30 -0500

    Updated resume with link to google drive

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 
-import ResumePDF from './Roberto_Basulto_Resume.pdf'
-
 const Resume = () => {
     return (
         <Container style={{ height: '90vh', width:'60%' }}>
-            <iframe src={ResumePDF} width="100%" height="100%" allow="autoplay"></iframe>
+            <iframe src="https://drive.google.com/file/d/1--QJ0jb48JaZjzPnSAebMCJF87WYHPCt/preview" width="100%" height="100%" allow="autoplay"></iframe>
         </Container>
     )
 }
